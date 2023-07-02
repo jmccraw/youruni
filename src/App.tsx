823b7478d8f48e71fcedb495af56c659a7e54e39
Justin McCraw <jmccraw1@gmail.com>
2023-07-01 21:58:01 -0400

    feat: add the Hero component and add to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Nav from './components/nav';
+import Hero from './components/hero';
 import Search from './components/search';
 
 function App() {
   return (
     <main>
       <Nav />
+      <Hero />
       <Search />
     </main>
   );
